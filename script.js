7ceca0f7854c9a4ef3725086298ee1cfae58a764
Kevin Monroy <kevins-monroyo@unilibre.edu.co>
2026-02-14 19:27:17 -0500

    agregando circulo y marcador

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById('map').style.height = "calc(100vh - 64px)";
 document.getElementById('map').style.width = "100%";
 
-var map = L.map('map').setView([51.505,-0.09],13);
+var map = L.map('map').setView([4.6097,-74.0817],12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
     maxZoom: 19,
@@ -16,3 +16,16 @@ window.addEventListener('load',function(){
         map.invalidateSize();
     },200);
 });
+
+
+
+// agregar un marcador
+
+var marker = L.marker([4.6097,-74.0817]).addTo(map);
+
+var circle = L.circle([4.6099,-74.0819],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
